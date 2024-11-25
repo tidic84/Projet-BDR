1c737f8d98807628da0dacf386516be49b99c029
uapv2400993 <uapv2400993@e-mc127-13.ad.univ-avignon.fr>
2024-11-25 14:30:23 +0100

    corpus

--- src/bdr/Corpus.java
+++ src/bdr/Corpus.java
@@ -1,11 +1,34 @@
 package bdr;
 import java.util.Vector;
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
 
 public class Corpus extends Vector<Document> {
-	private String titre;
-	
-	public Corpus(String path, DataSets dataSets) {
-		
-		
-	}
+    private String titre;
+    private Vector<Document>collDocuments ;
+
+    public Corpus(String path, DataSets dataSets) {
+
+        collDocuments=new Vector<Document>();
+
+         try {
+              File myObj = new File(path);
+              Scanner myReader = new Scanner(myObj);
+
+              while (myReader.hasNextLine())
+              {
+                String data = myReader.nextLine();
+                String[] splitted = data.split("|||");
+                System.out.println(splitted[0]);
+              //  collDocuments.add(new Document(new Mot (data)));
+
+              }
+              myReader.close();
+            } catch (FileNotFoundException e) {
+              System.out.println("An error occurred.");
+              e.printStackTrace();
+            }
+
+    }
 }
--- src/bdr/Main.java
+++ src/bdr/Main.java
@@ -3,16 +3,8 @@ package bdr;
 public class Main {
 
 	public static void main(String[] args) {
-		Mot titre = new Mot ("TOTO");
-		Document document = new Document (titre);
-		document.putMot("cc");
-		document.putMot("BGYDZGI");
-
-		document.putMot("MOT");
-
-		document.putMot("KI");
-
-System.out.println(document);
+		DataSets dataSets = null;
+		Corpus corpus = new Corpus("./ressources/stemmed.txt", dataSets);
 
 	}
 
