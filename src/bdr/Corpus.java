7810ff2e467f8b0397bf408bc7581920e07e5630
Tidic <tidic84@gmail.com>
2024-11-26 22:57:44 +0100

    opti

--- src/bdr/Corpus.java
+++ src/bdr/Corpus.java
@@ -5,66 +5,66 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 
 public class Corpus extends Vector<Document> {
-    private String titre;
-    private Vector<Document>collDocuments ;
+	private String titre;
+	private Vector<Document>collDocuments ;
 
-    public Corpus(String path, DataSets dataSets) {
+	public Corpus(String path, DataSets dataSets) {
 
-        collDocuments=new Vector<Document>();
-        int cpt = 0;
+		collDocuments=new Vector<Document>();
+		int cpt = 0;// ENLEVER
 
-         try {
-              File myObj = new File(path);
-              Scanner myReader = new Scanner(myObj);
+		try {
+			File myObj = new File(path);
+			Scanner myReader = new Scanner(myObj);
+			String data = myReader.nextLine();
+			String titre = "";
+			Document document = null;
 
-              while (myReader.hasNextLine() && cpt < 10000)
-              {
-            	  switch (dataSets) {
-            	  case DataSets.WIKIPEDIA:
-                      String[] data = myReader.nextLine().split("\\|\\|\\|");
-                    Document document = new Document(new Mot (data[0]));
-                    collDocuments.add(document);
-              	  cpt++;
-                    break;
-            	  case DataSets.OUVRAGES:
-                      String[] datao = myReader.nextLine().split("{");
-                    Document ouvrages = new Document(new Mot (datao[0]));
-                    collDocuments.add(ouvrages);
-              	  cpt++;
-                    	
-            	  }
+			while (myReader.hasNextLine() && cpt < 1000)
+			{
+				switch (dataSets) {
+				case DataSets.WIKIPEDIA:
+					titre = data.split("\\|\\|\\|")[0];
+					document = new Document(new Mot(titre));
+					collDocuments.add(document);
+					cpt++; // ENLEVER
+					break;
+				case DataSets.OUVRAGES:
+					titre = data.split("\\{")[0];
+					document = new Document(new Mot(titre));
+					collDocuments.add(document);
+					cpt++; // ENLEVER
+				}
+
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+		titre = path ;
+
+	}
 
 
+	public void addDocument(String path , DataSets dataSets) {
+		try {
+			File doc = new File(path);
+			Scanner scandoc = new Scanner (doc);
 
-              }
-              myReader.close();
-            } catch (FileNotFoundException e) {
-              System.out.println("An error occurred.");
-              e.printStackTrace();
-            }
-         
-         titre = path ;
+			while ( scandoc.hasNextLine());
+			{
+				String[] data = scandoc.nextLine().split("\\|\\|\\|");    	
+				collDocuments.add(new Document(new Mot (data[0])));
+			}
+			scandoc.close();	
+		}catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 
-    }
-    
-    
-    public void addDocument(String path , DataSets dataSets) {
-    	try {
-    	 File doc = new File(path);
-         Scanner scandoc = new Scanner (doc);
-    			
-    	while ( scandoc.hasNextLine());
-    			{
-    	String[] data = scandoc.nextLine().split("\\|\\|\\|");    	
-    	collDocuments.add(new Document(new Mot (data[0])));
-    	}
-    	scandoc.close();	
-    	}catch (FileNotFoundException e) {
-    	              System.out.println("An error occurred.");
-    	              e.printStackTrace();
-    	            }
-
-    }
+	}
 
 
 	@Override
--- src/bdr/Test.java
+++ src/bdr/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Corpus corpus = new Corpus("./ressources/stemmed.txt", DataSets.WIKIPEDIA);
+		Corpus corpus = new Corpus("./ressources/booksummaries.txt", DataSets.OUVRAGES);
 		System.out.println(corpus);
 
 	}
