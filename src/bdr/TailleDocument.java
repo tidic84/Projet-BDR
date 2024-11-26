2ebb6cc17b61a5a3e1ac0e4b6ef0036ed6fd6059
Tyziryx <alexim13550@gmail.com>
2024-11-26 23:09:53 +0100

    part2

---
+++ src/bdr/TailleDocument.java
@@ -0,0 +1,21 @@
+package bdr;
+
+public class TailleDocument extends Corpus {
+
+	public TailleDocument(String path, DataSets dataSets) {
+		super(path, dataSets);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void calculer(Corpus corpus) {
+	int n=0;
+	
+	
+		
+	}
+	
+	
+	
+
+}
